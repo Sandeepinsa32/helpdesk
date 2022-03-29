07b2c4b537d7eb8f6a82155b3bc617bea2de9ffc
Prince Bansal <1726250.cse.coe@cgc.edu.in>
2022-03-29 15:32:48 +0530

    Login and register screen fixed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import MainComponent from './MainComponet';
-import Login from './views/login';
-import {Account} from './views/Account';
-import {StyledEngineProvider} from '@mui/material/styles';
-import RouteComponents from './RouteComponents';
+import MainComponent from "./MainComponet";
+import Login from "./views/login";
+import { Account } from "./views/Account";
+import { StyledEngineProvider } from "@mui/material/styles";
+import RouteComponents from "./RouteComponents";
 
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
-	return (
-		<>
-			<StyledEngineProvider>
-				<Router>
-					<MainComponent />
-					{/* <Account /> */}
-				</Router>
-			</StyledEngineProvider>
-		</>
-	);
+  return (
+    <>
+      <Router>
+        <StyledEngineProvider>
+          <Routes>
+            <Route path="/register" element={<Login />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/*" element={<MainComponent />} />
+          </Routes>
+          {/* <MainComponent /> */}
+          {/* <Account /> */}
+        </StyledEngineProvider>
+      </Router>
+    </>
+  );
 }
 
 export default App;
--- src/MainComponet.jsx
+++ src/MainComponet.jsx
@@ -1,14 +1,14 @@
-import {ThemeProvider} from '@mui/material/styles';
-import {CssBaseline, StyledEngineProvider} from '@mui/material';
-import {DashboardLayout} from './dashboard-layout';
-import MiniDrawer from './drawer';
+import { ThemeProvider } from "@mui/material/styles";
+import { CssBaseline, StyledEngineProvider } from "@mui/material";
+import { DashboardLayout } from "./dashboard-layout";
+import MiniDrawer from "./drawer";
 
 export default function MainComponent() {
-	return (
-		<>
-			{/* <h1>hello world</h1> */}
-			<MiniDrawer></MiniDrawer>
-			{/* <DashboardLayout></DashboardLayout> */}
-		</>
-	);
+  return (
+    <>
+      {/* <h1>hello world</h1> */}
+      <MiniDrawer></MiniDrawer>
+      {/* <DashboardLayout></DashboardLayout> */}
+    </>
+  );
 }
