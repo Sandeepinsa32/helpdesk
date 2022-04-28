4384e9e1317a64b1b15179c6d886123a5f0fd818
Prince Bansal <1726250.cse.coe@cgc.edu.in>
2022-04-28 14:03:00 +0530

    fixes

--- src/views/components/Form/PaymentForm.jsx
+++ src/views/components/Form/PaymentForm.jsx
@@ -1,153 +1,204 @@
-import React, {useState} from 'react';
-import {useFormik} from 'formik';
-import * as Yup from 'yup';
-import {Typography, Link, Grid, TextField, Button} from '@mui/material';
+import React, { useState } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import {
+  Typography,
+  Link,
+  Paper,
+  Grid,
+  TextField,
+  Button,
+} from "@mui/material";
 
 // mui ICon
-import AddIcon from '@mui/icons-material/Add';
-import RemoveIcon from '@mui/icons-material/Remove';
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
 
-export default function PaymentForm({setCardInfo}) {
-	const [inputList, setInputList] = useState([{cardName: '', cardNumber: '', expiryDate: '', cvv: ''}]);
+export default function PaymentForm({ setCardInfo }) {
+  const [inputList, setInputList] = useState([
+    { cardName: "", cardNumber: "", expiryDate: "", cvv: "" },
+  ]);
 
-	// handle input change
-	const handleInputChange = (e, index) => {
-		const {name, value} = e.target;
-		const list = [...inputList];
-		list[index][name] = value;
-		setInputList(list);
-	};
+  // handle input change
+  const handleInputChange = (e, index) => {
+    const { name, value } = e.target;
+    const list = [...inputList];
+    list[index][name] = value;
+    setInputList(list);
+  };
 
-	// handle click event of the Remove button
-	const handleRemoveClick = (index) => {
-		const list = [...inputList];
-		list.splice(index, 1);
-		setInputList(list);
-	};
+  // handle click event of the Remove button
+  const handleRemoveClick = (index) => {
+    const list = [...inputList];
+    list.splice(index, 1);
+    setInputList(list);
+  };
 
-	// handle click event of the Add button
-	const handleAddClick = () => {
-		setInputList([...inputList, {cardName: '', cardNumber: '', expiryDate: '', cvv: ''}]);
-	};
+  // handle click event of the Add button
+  const handleAddClick = () => {
+    setInputList([
+      ...inputList,
+      { cardName: "", cardNumber: "", expiryDate: "", cvv: "" },
+    ]);
+  };
 
-	const formik = useFormik({
-		initialValues: {
-			cardDetail: {inputList},
-			comments: '',
-			notes: '',
-		},
-		validationSchema: Yup.object({
-			cardName: Yup.string().max(255).required('CardHolder name is required'),
-			cardNumber: Yup.number(18).required().positive().integer().required('Card Number is required'),
-			expiryDate: '',
-			cvv: Yup.number(4).required().positive().integer().required('pricePerPax is required'),
-		}),
-		onSubmit: (values) => {
-			alert('done');
-		},
-	});
-	return (
-		<>
-			<Typography variant='h6' gutterBottom>
-				Payment method
-			</Typography>
+  const formik = useFormik({
+    initialValues: [
+      {
+        cardDetail: { inputList },
+        comments: "",
+        notes: "",
+      },
+    ],
+    // validationSchema: Yup.object({
+    //   cardName: Yup.string().max(255).required("CardHolder name is required"),
+    //   cardNumber: Yup.number(18)
+    //     .required()
+    //     .positive()
+    //     .integer()
+    //     .required("Card Number is required"),
+    //   expiryDate: "",
+    //   cvv: Yup.number(4)
+    //     .required()
+    //     .positive()
+    //     .integer()
+    //     .required("pricePerPax is required"),
+    // }),
+    onSubmit: (values) => {
+      console.log(values);
+      alert("done");
+    },
+  });
+  return (
+    <>
+      <Typography variant="h6" gutterBottom>
+        Payment method
+      </Typography>
 
-			{inputList.map((x, i) => {
-				return (
-					<Grid container spacing={3}>
-						<Grid item xs={12} md={6}>
-							<TextField
-								required
-								name='cardName'
-								label='Name on card'
-								fullWidth
-								autoComplete='cc-name'
-								variant='standard'
-								onChange={(e) => {
-									handleInputChange(e, i);
-									formik.handleChange();
-								}}
-								error={Boolean(formik.touched.cardName && formik.errors.cardName)}
-								helperText={formik.touched.cardName && formik.errors.cardName}
-								onBlur={formik.handleBlur}
-								// onChange={}
-								value={formik.values.cardName}
-							/>
-						</Grid>
-						<Grid item xs={12} md={6}>
-							<TextField
-								required
-								name='cardNumber'
-								label='Card number'
-								fullWidth
-								autoComplete='cc-number'
-								variant='standard'
-								// onChange={(e) => handleInputChange(e, i)}
-								error={Boolean(formik.touched.cardNumber && formik.errors.cardNumber)}
-								helperText={formik.touched.cardNumber && formik.errors.cardNumber}
-								onBlur={formik.handleBlur}
-								onChange={formik.handleChange}
-								value={formik.values.cardNumber}
-							/>
-						</Grid>
-						<Grid item xs={12} md={6}>
-							<TextField
-								required
-								name='expiryDate'
-								label='Expiry date'
-								fullWidth
-								autoComplete='cc-exp'
-								variant='standard'
-								// onChange={(e) => handleInputChange(e, i)}
-								error={Boolean(formik.touched.expiryDate && formik.errors.expiryDate)}
-								helperText={formik.touched.expiryDate && formik.errors.expiryDate}
-								onBlur={formik.handleBlur}
-								onChange={formik.handleChange}
-								value={formik.values.expiryDate}
-							/>
-						</Grid>
-						<Grid item xs={12} md={6}>
-							<TextField
-								required
-								name='cvv'
-								label='CVV'
-								fullWidth
-								autoComplete='cc-csc'
-								variant='standard'
-								// onChange={(e) => handleInputChange(e, i)}
-								error={Boolean(formik.touched.cvv && formik.errors.cvv)}
-								helperText={'Last three digits on signature strip' || (formik.touched.cvv && formik.errors.cvv)}
-								onBlur={formik.handleBlur}
-								onChange={formik.handleChange}
-								value={formik.values.cvv}
-							/>
-						</Grid>
+      {inputList.map((x, i) => {
+        return (
+          <Paper
+            style={{
+              padding: 12,
+              margin: 8,
+            }}
+          >
+            <Grid container spacing={3}>
+              <Grid item xs={12} md={6}>
+                <TextField
+                  required
+                  name="cardName"
+                  label="Name on card"
+                  fullWidth
+                  autoComplete="cc-name"
+                  variant="standard"
+                  onChange={(e) => {
+                    handleInputChange(e, i);
+                    formik.handleChange();
+                  }}
+                  error={Boolean(
+                    formik.touched.cardName && formik.errors.cardName
+                  )}
+                  helperText={formik.touched.cardName && formik.errors.cardName}
+                  onBlur={formik.handleBlur}
+                  // onChange={}
+                  value={formik.values.cardName}
+                />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <TextField
+                  required
+                  name="cardNumber"
+                  label="Card number"
+                  fullWidth
+                  autoComplete="cc-number"
+                  variant="standard"
+                  // onChange={(e) => handleInputChange(e, i)}
+                  error={Boolean(
+                    formik.touched.cardNumber && formik.errors.cardNumber
+                  )}
+                  helperText={
+                    formik.touched.cardNumber && formik.errors.cardNumber
+                  }
+                  onBlur={formik.handleBlur}
+                  onChange={formik.handleChange}
+                  value={formik.values.cardNumber}
+                />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <TextField
+                  required
+                  name="expiryDate"
+                  label="Expiry date"
+                  fullWidth
+                  autoComplete="cc-exp"
+                  variant="standard"
+                  // onChange={(e) => handleInputChange(e, i)}
+                  error={Boolean(
+                    formik.touched.expiryDate && formik.errors.expiryDate
+                  )}
+                  helperText={
+                    formik.touched.expiryDate && formik.errors.expiryDate
+                  }
+                  onBlur={formik.handleBlur}
+                  onChange={formik.handleChange}
+                  value={formik.values.expiryDate}
+                />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <TextField
+                  required
+                  name="cvv"
+                  label="CVV"
+                  fullWidth
+                  autoComplete="cc-csc"
+                  variant="standard"
+                  // onChange={(e) => handleInputChange(e, i)}
+                  error={Boolean(formik.touched.cvv && formik.errors.cvv)}
+                  helperText={
+                    "Last three digits on signature strip" ||
+                    (formik.touched.cvv && formik.errors.cvv)
+                  }
+                  onBlur={formik.handleBlur}
+                  onChange={formik.handleChange}
+                  value={formik.values.cvv}
+                />
+              </Grid>
 
-						{inputList.length !== 1 && (
-							<>
-								<Grid item xs={12} md={10}></Grid>
+              {inputList.length !== 1 && (
+                <>
+                  <Grid item xs={12} md={10}></Grid>
 
-								<Grid item xs={12} md={2}>
-									<Button startIcon={<RemoveIcon fontSize='small' />} onClick={() => handleRemoveClick(i)} sx={{mr: 1}}>
-										Remove
-									</Button>
-								</Grid>
-							</>
-						)}
-						{inputList.length - 1 === i && (
-							<>
-								<Grid item xs={12} md={8}></Grid>
-								<Grid item xs={12} md={4}>
-									<Button startIcon={<AddIcon fontSize='small' />} onClick={handleAddClick} sx={{mr: 1}}>
-										Add One More Card
-									</Button>
-								</Grid>
-							</>
-						)}
-					</Grid>
-				);
-			})}
-			<div style={{marginTop: 20}}>{JSON.stringify(formik.value)}</div>
-		</>
-	);
+                  <Grid item xs={12} md={2}>
+                    <Button
+                      startIcon={<RemoveIcon fontSize="small" />}
+                      onClick={() => handleRemoveClick(i)}
+                      sx={{ mr: 1 }}
+                    >
+                      Remove
+                    </Button>
+                  </Grid>
+                </>
+              )}
+              {inputList.length - 1 === i && (
+                <>
+                  <Grid item xs={12} md={8}></Grid>
+                  <Grid item xs={12} md={4}>
+                    <Button
+                      startIcon={<AddIcon fontSize="small" />}
+                      onClick={handleAddClick}
+                      sx={{ mr: 1 }}
+                    >
+                      Add One More Card
+                    </Button>
+                  </Grid>
+                </>
+              )}
+            </Grid>
+          </Paper>
+        );
+      })}
+      <div style={{ marginTop: 20 }}>{JSON.stringify(formik.value)}</div>
+    </>
+  );
 }
--- src/views/components/addUser/userSearch.js
+++ src/views/components/addUser/userSearch.js
@@ -1,72 +1,91 @@
-import {React, useState} from 'react';
-import {Box, Button, Card, CardContent, TextField, ThemeProvider, InputAdornment, SvgIcon, Typography} from '@mui/material';
-import CloseIcon from '@mui/icons-material/Close';
+import { React, useState } from "react";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  TextField,
+  ThemeProvider,
+  InputAdornment,
+  SvgIcon,
+  Typography,
+} from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 
 //  local icon
-import {Search as SearchIcon} from '../../../assets/icons/search';
-import {Upload as UploadIcon} from '../../../assets/icons/upload';
-import {Download as DownloadIcon} from '../../../assets/icons/download';
+import { Search as SearchIcon } from "../../../assets/icons/search";
+import { Upload as UploadIcon } from "../../../assets/icons/upload";
+import { Download as DownloadIcon } from "../../../assets/icons/download";
 
-import {createTheme} from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles";
 
 const style = {
-	position: 'absolute',
-	top: '50%',
-	left: '50%',
-	transform: 'translate(-50%, -50%)',
-	width: 'auto',
-	height: 'auto',
-	bgcolor: 'background.paper',
-	// border: '2px solid #000',
-	boxShadow: 24,
-	borderRadius: '1rem',
-	p: 4,
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "auto",
+  height: "auto",
+  bgcolor: "background.paper",
+  // border: '2px solid #000',
+  boxShadow: 24,
+  borderRadius: "1rem",
+  p: 4,
 };
 const theme = createTheme({
-	palette: {
-		neutral: {
-			main: '#64748B',
-			contrastText: '#fff',
-		},
-	},
+  palette: {
+    neutral: {
+      main: "#64748B",
+      contrastText: "#fff",
+    },
+  },
 });
 export const Search = (props) => {
-	return (
-		<Box>
-			<ThemeProvider theme={theme}>
-				{/* Search COmponent */}
-				<Box sx={{mt: 3}}>
-					<Card>
-						<CardContent>
-							<Box fullWidth sx={{display: 'flex'}}>
-								<TextField
-									size='small'
-									sx={{width: `26vw`, height: `2rem`}}
-									InputProps={{
-										startAdornment: (
-											<InputAdornment position='start'>
-												<SvgIcon color='action' fontSize='small'>
-													<SearchIcon />
-												</SvgIcon>
-											</InputAdornment>
-										),
-									}}
-									placeholder='Search customer'
-									variant='outlined'
-								/>
-								<Box sx={{px: 2, mt: 0.5}}>
-									<Button sx={{textTransform: 'capitalize', mx: 2}} size='small' variant='contained'>
-										Search
-									</Button>
-									<Button sx={{textTransform: 'capitalize', mx: 2}} size='small' variant='contained' color='neutral'>
-										Reset
-									</Button>
-								</Box>
-							</Box>
-						</CardContent>
-					</Card>
-				</Box>
-			</ThemeProvider>
-		</Box>
-	);
+  return (
+    <Box>
+      <ThemeProvider theme={theme}>
+        {/* Search COmponent */}
+        <Box sx={{ mt: 3 }}>
+          <Card>
+            <CardContent>
+              <Box fullWidth sx={{ display: "flex" }}>
+                <TextField
+                  size="small"
+                  sx={{ width: `26vw`, height: `2rem` }}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <SvgIcon color="action" fontSize="small">
+                          <SearchIcon />
+                        </SvgIcon>
+                      </InputAdornment>
+                    ),
+                  }}
+                  placeholder="Search customer"
+                  variant="outlined"
+                />
+                <Box sx={{ px: 2, mt: 0.5 }}>
+                  <Button
+                    sx={{ textTransform: "capitalize", mx: 2 }}
+                    size="small"
+                    variant="contained"
+                  >
+                    Search
+                  </Button>
+                  <Button
+                    sx={{ textTransform: "capitalize", mx: 2 }}
+                    size="small"
+                    variant="contained"
+                    color="neutral"
+                  >
+                    Reset
+                  </Button>
+                </Box>
+              </Box>
+            </CardContent>
+          </Card>
+        </Box>
+      </ThemeProvider>
+    </Box>
+  );
 };
