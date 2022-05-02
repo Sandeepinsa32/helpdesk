863accfbe3efd42a5ee3ee65eb612568402f5e0b
sandeep <sandeepinsa32@gmail.com>
2022-05-02 23:00:34 +0530

    Feat : adding multiple field in email template

--- src/views/Email.jsx
+++ src/views/Email.jsx
@@ -57,172 +57,177 @@ const Email = () => {
 	};
 	return (
 		<>
-			<Box sx={{height: '100vh', overflow: 'scroll'}}>
+			<Box>
 				<>
-					<Card sx={{height: '100%'}}>
-						<CardContent>
-							<Box
-								sx={{
-									display: 'flex',
-									flexWrap: 'nowrap',
-									p: 1,
-									m: 1,
-									bgcolor: 'background.paper',
-									width: 1,
-									height: 1,
-									borderRadius: 1,
-								}}>
-								<Box sx={{width: '50%'}}>
-									<Grid container spacing={3} sx={{justifyContent: 'space-between'}}>
-										<Grid item xs={12} sm={4}>
-											<FormControl required fullWidth>
-												<InputLabel id='Email-template-Dropdown-label'>Email Template</InputLabel>
-												<Select
-													labelId='Email-template-Dropdown-label	'
-													id='Email-template-Dropdown'
-													value={selectedEmailTemplate}
-													onChange={handleEmailTemplateChange}
-													fullWidth
-													name='emailTemplate'
-													label='Email Template'>
-													<MenuItem value='1'>1</MenuItem>
-													<MenuItem value='2'>2</MenuItem>
-													<MenuItem value='3'>3</MenuItem>
-													<MenuItem value='4'>4</MenuItem>
-													<MenuItem value='5'>5</MenuItem>
-												</Select>
-											</FormControl>
-										</Grid>
-
-										<Grid item xs={12} sm={4}>
-											<FormControl required fullWidth>
-												<InputLabel id='Input-count-Dropdown-label'>No. Of Passenger</InputLabel>
-												<Select
-													labelId='Input-count-Dropdown-label	'
-													id='Input-count-Dropdown'
-													value={selectedInputCount}
-													onChange={handleInputCountChange}
-													fullWidth
-													name='inputCount'
-													label='No. Of Passenger'>
-													<MenuItem value='1'>1</MenuItem>
-													<MenuItem value='2'>2</MenuItem>
-													<MenuItem value='3'>3</MenuItem>
-													<MenuItem value='4'>4</MenuItem>
-													<MenuItem value='5'>5</MenuItem>
-												</Select>
-											</FormControl>
-										</Grid>
-										<Grid item xs={12} sm={2}></Grid>
-										<Grid item xs={12} md={4} sx={{mb: 2}}>
-											<LocalizationProvider fullWidth dateAdapter={AdapterDateFns}>
-												<DatePicker
-													fullWidth
-													name='expiryDate'
-													label='Expiry date'
-													inputFormat='dd/MM/yyyy'
-													onChange={(newValue) => {
-														setBookingDateValue(
-															new Date(newValue).toLocaleDateString('en-US', {
-																day: '2-digit',
-																month: '2-digit',
-																year: 'numeric',
-															})
-														);
-														console.log(
-															new Date(newValue).toLocaleDateString('en-US', {
-																day: '2-digit',
-																month: '2-digit',
-																year: 'numeric',
-															})
-														);
-													}}
-													value={bookingDateValue}
-													renderInput={(params) => <TextField {...params} />}
-												/>
-											</LocalizationProvider>
-										</Grid>
-										<Grid item xs={12} md={4}>
-											<TextField
-												required
-												name='name'
-												label='Name'
+					<Box
+						sx={{
+							display: 'flex',
+							flexWrap: 'nowrap',
+							p: 0,
+							m: 0,
+							width: 1,
+							height: 1,
+							borderRadius: 1,
+						}}>
+						<Box sx={{width: '59%'}}>
+							<Paper elevation={3} sx={{height: 1}}>
+								<Grid container spacing={1} sx={{m: 0, p: 1}}>
+									{/*  Email template  */}
+									<Grid item md={4}>
+										<FormControl required fullWidth>
+											<InputLabel id='Email-template-Dropdown-label'>Email Template</InputLabel>
+											<Select
+												labelId='Email-template-Dropdown-label	'
+												id='Email-template-Dropdown'
+												value={selectedEmailTemplate}
+												onChange={handleEmailTemplateChange}
 												fullWidth
-												autoComplete='name'
-												onChange={(e) => {
-													// handleInputChange(e);
-													setName(e.target.value);
-												}}
-												value={name}
-											/>
-										</Grid>
-										<Grid item xs={12} sm={2}></Grid>
-										<Grid item xs={12} md={4}>
-											<TextField
-												required
-												name='totalAmount'
-												label='totalAmount'
-												fullWidth
-												onChange={(e) => {
-													setTotalAmt(e.target.value);
-												}}
-												value={totalAmt}
-											/>
-										</Grid>
+												name='emailTemplate'
+												label='Email Template'>
+												<MenuItem value='1'>1</MenuItem>
+												<MenuItem value='2'>2</MenuItem>
+												<MenuItem value='3'>3</MenuItem>
+												<MenuItem value='4'>4</MenuItem>
+												<MenuItem value='5'>5</MenuItem>
+											</Select>
+										</FormControl>
 									</Grid>
 
-									<Grid container spacing={3}>
-										{/* {inputList.map((x, i) => {
-											return (
-												<>
-													<Grid item xs={12} md={4}>
-														<TextField
-															required
-															name='name'
-															label='Name'
-															fullWidth
-															autoComplete='name'
-															onChange={(e) => {
-																handleInputChange(e, i);
-															}}
-															value={inputList[i].cardName}
-														/>
-													</Grid>
+									{/*   Passenger Count  */}
 
-													{inputList.length !== 1 && (
-														<>
-															<Grid item xs={12} md={10}></Grid>
+									{/* <Grid item xs={12} md={4}>
+										<FormControl required fullWidth>
+											<InputLabel id='Input-count-Dropdown-label'>No. Of Passenger</InputLabel>
+											<Select
+												labelId='Input-count-Dropdown-label	'
+												id='Input-count-Dropdown'
+												value={selectedInputCount}
+												onChange={handleInputCountChange}
+												fullWidth
+												name='inputCount'
+												label='No. Of Passenger'>
+												<MenuItem value='1'>1</MenuItem>
+												<MenuItem value='2'>2</MenuItem>
+												<MenuItem value='3'>3</MenuItem>
+												<MenuItem value='4'>4</MenuItem>
+												<MenuItem value='5'>5</MenuItem>
+											</Select>
+										</FormControl>
+									</Grid> */}
 
-															<Grid item xs={12} md={2}>
-																<Button startIcon={<RemoveIcon fontSize='small' />} onClick={() => handleRemoveClick(i)} sx={{mr: 1}}>
-																	Remove
-																</Button>
-															</Grid>
-														</>
-													)}
-													{inputList.length - 1 === i && (
-														<>
-															<Grid item xs={12} md={8}></Grid>
-															<Grid item xs={12} md={4}>
-																<Button startIcon={<AddIcon fontSize='small' />} onClick={handleAddClick} sx={{mr: 1}}>
-																	Add One More Card
-																</Button>
-															</Grid>
-														</>
-													)}
-												</>
-											);
-										})} */}
+									{/*  Date Picker */}
+									<Grid item xs={12} md={4} sx={{mb: 2}}>
+										<LocalizationProvider fullWidth dateAdapter={AdapterDateFns}>
+											<DatePicker
+												fullWidth
+												name='expiryDate'
+												label='Expiry date'
+												inputFormat='dd/MM/yyyy'
+												onChange={(newValue) => {
+													setBookingDateValue(
+														new Date(newValue).toLocaleDateString('en-US', {
+															day: '2-digit',
+															month: '2-digit',
+															year: 'numeric',
+														})
+													);
+													console.log(
+														new Date(newValue).toLocaleDateString('en-US', {
+															day: '2-digit',
+															month: '2-digit',
+															year: 'numeric',
+														})
+													);
+												}}
+												value={bookingDateValue}
+												renderInput={(params) => <TextField {...params} />}
+											/>
+										</LocalizationProvider>
 									</Grid>
-								</Box>
-								<Box sx={{width: '50%'}}>
-									<Paper elevation={3} sx={{height: 1}}>
-										<Email1 selectedEmailTemplate={selectedEmailTemplate} name={name} bookingDate={bookingDateValue} TotalAmount={totalAmt} noOfPas={selectedInputCount} />
-									</Paper>
-								</Box>
-							</Box>
-						</CardContent>
-					</Card>
+
+									{/* Total Amount */}
+									<Grid item xs={12} md={3} sx={{pr: 1}}>
+										<TextField
+											required
+											name='totalAmount'
+											label='totalAmount'
+											fullWidth
+											onChange={(e) => {
+												setTotalAmt(e.target.value);
+											}}
+											value={totalAmt}
+										/>
+									</Grid>
+
+									{/* Name Amount */}
+									{/* <Grid item xs={12} md={4}>
+										<TextField
+											required
+											name='name'
+											label='Name'
+											fullWidth
+											autoComplete='name'
+											onChange={(e) => {
+												// handleInputChange(e);
+												setName(e.target.value);
+											}}
+											value={name}
+										/>
+									</Grid> */}
+
+									{inputList.map((x, i) => {
+										return (
+											<>
+												<Grid item xs={12} md={4}>
+													<TextField
+														required
+														name='name'
+														label='Name'
+														fullWidth
+														autoComplete='name'
+														onChange={(e) => {
+															handleInputChange(e, i);
+														}}
+														value={inputList[i].cardName}
+													/>
+												</Grid>
+
+												{inputList.length !== 1 && (
+													<>
+														<Grid item xs={12} md={2}>
+															<Button startIcon={<RemoveIcon fontSize='small' />} onClick={() => handleRemoveClick(i)} sx={{mr: 1, textTransform: 'capitalize'}}>
+																Remove
+															</Button>
+														</Grid>
+													</>
+												)}
+												{inputList.length - 1 === i && inputList.length < 9 && (
+													<>
+														<Grid item xs={12} md={10}></Grid>
+														<Grid item xs={12} md={2}>
+															<Button startIcon={<AddIcon fontSize='small' />} onClick={handleAddClick} sx={{mr: 1}}>
+																Add new
+															</Button>
+														</Grid>
+													</>
+												)}
+											</>
+										);
+									})}
+								</Grid>
+								<Box>{JSON.stringify(inputList)}</Box>
+							</Paper>
+						</Box>
+
+						<Box sx={{width: '1%'}}></Box>
+						<Box sx={{width: '40%'}}>
+							<Paper elevation={3} sx={{height: 1}}>
+								<Email1 selectedEmailTemplate={selectedEmailTemplate} name={name} bookingDate={bookingDateValue} TotalAmount={totalAmt} noOfPas={selectedInputCount} />
+							</Paper>
+						</Box>
+					</Box>
+					{/* </CardContent>
+					</Card> */}
 				</>
 			</Box>
 		</>
