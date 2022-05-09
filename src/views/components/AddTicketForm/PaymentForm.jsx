d1e4f5e66ffe78fd65d5708e09f1424eec08b325
sandeep <sandeepinsa32@gmail.com>
2022-05-09 11:30:10 +0530

    Feat :  payment  form

--- src/views/components/AddTicketForm/PaymentForm.jsx
+++ src/views/components/AddTicketForm/PaymentForm.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import {useFormik} from 'formik';
 import * as Yup from 'yup';
-import {Typography, Link, Paper, Grid, TextField, Button} from '@mui/material';
+import {Typography, Link, Paper, Grid, TextField, Button, FormControlLabel, Checkbox} from '@mui/material';
 import {AdapterDateFns} from '@mui/x-date-pickers/AdapterDateFns';
 import {DatePicker} from '@mui/x-date-pickers/DatePicker';
 
@@ -13,7 +13,7 @@ import RemoveIcon from '@mui/icons-material/Remove';
 
 export default function PaymentForm({setCardInfo, handleBack, handleSubmit}) {
 	const [inputList, setInputList] = useState([{cardName: '', cardHolderPhn: '', cardNumber: '', expiryDate: null, cvv: ''}]);
-	const [expiryDateValue, setExpiryDateValue] = useState(null);
+	const [isCompanyCard, setIsCompanyCard] = useState(false);
 
 	// handle input change
 	const handleInputChange = (e, index) => {
@@ -49,98 +49,174 @@ export default function PaymentForm({setCardInfo, handleBack, handleSubmit}) {
 			<Typography variant='h6' gutterBottom>
 				Payment method
 			</Typography>
+
 			<Grid container spacing={3}>
 				{inputList.map((x, i) => {
 					return (
-						<Paper
-							elevation={3}
-							style={{
-								padding: 12,
-								margin: 18,
-								marginLeft: '21px',
-								marginBorder: '0px',
-							}}>
-							<Grid container spacing={3}>
-								<Grid item xs={12} md={4}>
-									<TextField
-										required
-										name='cardName'
-										label='Name on card'
-										fullWidth
-										autoComplete='cc-name'
-										onChange={(e) => {
-											handleInputChange(e, i);
-										}}
-										value={inputList[i].cardName}
-									/>
-								</Grid>
-								<Grid item xs={12} md={4}>
-									<TextField required name='cardHolderPhn' label='Phone no.' fullWidth onChange={(e) => handleInputChange(e, i)} value={inputList[i].cardNumber} />
-								</Grid>
-								<Grid item xs={12} md={4}>
-									<TextField
-										required
-										name='cardNumber'
-										label='Card number'
-										fullWidth
-										autoComplete='cc-number'
-										onChange={(e) => handleInputChange(e, i)}
-										value={inputList[i].cardNumber}
-									/>
-								</Grid>
-								<Grid item xs={12} md={3}>
-									<TextField required name='cvv' label='CVV' fullWidth autoComplete='cc-csc' onChange={(e) => handleInputChange(e, i)} value={inputList[i].cvv} />
-								</Grid>
-								<Grid item xs={12} md={4}>
-									<LocalizationProvider fullWidth dateAdapter={AdapterDateFns}>
-										<DatePicker
+						<>
+							<Paper
+								elevation={3}
+								style={{
+									padding: 12,
+									margin: 18,
+									marginLeft: '21px',
+									marginBorder: '0px',
+								}}>
+								<Grid container spacing={3}>
+									<Grid item xs={12} md={4}>
+										<TextField
+											required
+											name='cardName'
+											label='Name on card'
 											fullWidth
-											name='expiryDate'
-											label='Expiry date'
-											inputFormat='dd/MM/yyyy'
-											onChange={(newValue) => {
-												handleDateInputChange(
-													i,
-													new Date(newValue).toLocaleDateString('en-US', {
-														day: '2-digit',
-														month: '2-digit',
-														year: 'numeric',
-													})
-												);
-
-												// setExpiryDateValue(newValue);
+											autoComplete='cc-name'
+											onChange={(e) => {
+												handleInputChange(e, i);
 											}}
-											value={inputList[i].expiryDate}
-											renderInput={(params) => <TextField {...params} />}
+											value={inputList[i].cardName}
 										/>
-									</LocalizationProvider>
+									</Grid>
+									<Grid item xs={12} md={4}>
+										<TextField required name='cardHolderPhn' label='Phone no.' fullWidth onChange={(e) => handleInputChange(e, i)} value={inputList[i].cardNumber} />
+									</Grid>
+									<Grid item xs={12} md={4}>
+										<TextField
+											required
+											name='cardNumber'
+											label='Card number'
+											fullWidth
+											autoComplete='cc-number'
+											onChange={(e) => handleInputChange(e, i)}
+											value={inputList[i].cardNumber}
+										/>
+									</Grid>
+									<Grid item xs={12} md={3}>
+										<TextField required name='cvv' label='CVV' fullWidth autoComplete='cc-csc' onChange={(e) => handleInputChange(e, i)} value={inputList[i].cvv} />
+									</Grid>
+									<Grid item xs={12} md={4}>
+										<LocalizationProvider fullWidth dateAdapter={AdapterDateFns}>
+											<DatePicker
+												fullWidth
+												name='expiryDate'
+												label='Expiry date'
+												inputFormat='dd/MM/yyyy'
+												onChange={(newValue) => {
+													handleDateInputChange(
+														i,
+														new Date(newValue).toLocaleDateString('en-US', {
+															day: '2-digit',
+															month: '2-digit',
+															year: 'numeric',
+														})
+													);
+
+													// setExpiryDateValue(newValue);
+												}}
+												value={inputList[i].expiryDate}
+												renderInput={(params) => <TextField {...params} />}
+											/>
+										</LocalizationProvider>
+									</Grid>
+
+									{inputList.length !== 1 && (
+										<>
+											<Grid item xs={12} md={10}></Grid>
+
+											<Grid item xs={12} md={2}>
+												<Button startIcon={<RemoveIcon fontSize='small' />} onClick={() => handleRemoveClick(i)} sx={{mr: 1}}>
+													Remove
+												</Button>
+											</Grid>
+										</>
+									)}
+									{inputList.length - 1 === i && (
+										<>
+											<Grid item xs={12} md={8}></Grid>
+											<Grid item xs={12} md={6}>
+												<Button startIcon={<AddIcon fontSize='small' />} onClick={handleAddClick} sx={{mr: 1}}>
+													Add One More Card
+												</Button>
+											</Grid>
+										</>
+									)}
 								</Grid>
-
-								{inputList.length !== 1 && (
-									<>
-										<Grid item xs={12} md={10}></Grid>
-
-										<Grid item xs={12} md={2}>
-											<Button startIcon={<RemoveIcon fontSize='small' />} onClick={() => handleRemoveClick(i)} sx={{mr: 1}}>
-												Remove
-											</Button>
-										</Grid>
-									</>
-								)}
-								{inputList.length - 1 === i && (
-									<>
-										<Grid item xs={12} md={8}></Grid>
-										<Grid item xs={12} md={6}>
-											<Button startIcon={<AddIcon fontSize='small' />} onClick={handleAddClick} sx={{mr: 1}}>
-												Add One More Card
-											</Button>
-										</Grid>
-									</>
-								)}
-							</Grid>
-						</Paper>
+							</Paper>
+						</>
 					);
 				})}
+			</Grid>
+			<Grid container spacing={3}>
+				{/* <Paper
+					elevation={3}
+					style={{
+						padding: 12,
+						margin: 18,
+						marginLeft: '21px',
+						marginBorder: '0px',
+					}}> */}
+				<Grid container spacing={3} sx={{padding: `12px`, margin: 0}}>
+					<Grid item xs={12} md={12}>
+						<FormControlLabel
+							control={
+								<Checkbox
+									checked={isCompanyCard}
+									onChange={(e) => {
+										setIsCompanyCard(!isCompanyCard);
+									}}
+									name='companyCard'
+									color='primary'
+								/>
+							}
+							label='Company CC used ?'
+						/>
+					</Grid>
+					{isCompanyCard ? (
+						<>
+							<Grid item xs={12} md={4}>
+								<TextField
+									required
+									name='ccUsedCount'
+									label='CC used How many times? '
+									fullWidth
+
+									// onChange={(e) => {
+									// 	handleInputChange(e, i);
+									// }}
+									// value={inputList[i].cardName}
+								/>
+							</Grid>
+							<Grid item xs={12} md={4}>
+								<TextField
+									required
+									name='ccAmountUsed'
+									label=' Company CC Used Amount'
+									fullWidth
+
+									// onChange={(e) => {
+									// 	handleInputChange(e, i);
+									// }}
+									// value={inputList[i].cardName}
+								/>
+							</Grid>
+							<Grid item xs={12} md={4}>
+								<TextField
+									required
+									name='ccLastDigit'
+									label='Last 4 Digits of of Company CC '
+									fullWidth
+
+									// onChange={(e) => {
+									// 	handleInputChange(e, i);
+									// }}
+									// value={inputList[i].cardName}
+								/>
+							</Grid>
+						</>
+					) : null}
+				</Grid>
+				{/* </Paper> */}
+			</Grid>
+			<Grid container spacing={3}>
 				<Grid item xs={8} sm={9}></Grid>
 				<Grid item xs={4} sm={1}>
 					<Button
