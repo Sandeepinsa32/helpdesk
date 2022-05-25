4ac627638399d707d52f6514b463a53bb53f93b6
Prince Bansal <1726250.cse.coe@cgc.edu.in>
2022-05-25 11:06:10 +0530

    added loader to logs

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -1,5 +1,5 @@
 import { toast } from "react-hot-toast";
-// export const BASEURL = 'https://helpdeskcrm.herokuapp.com';
+// export const BASEURL = "https://helpdeskcrm.herokuapp.com";
 export const BASEURL = "http://localhost:4000";
 
 export const successToast = (message) => toast.success(message);
--- src/views/components/ViewLog.js
+++ src/views/components/ViewLog.js
@@ -6,6 +6,7 @@ import {
   ListItemButton,
   ListItemText,
   ListItemIcon,
+  CircularProgress,
   List,
 } from "@mui/material";
 import axios from "axios";
@@ -16,11 +17,18 @@ const ViewLog = ({ id }) => {
   id = id._id;
   console.log(id);
 
+  const [isLoading, setIsLoading] = useState(true);
   const handleLoadLogs = async (id) => {
     axios
       .get(BASEURL + "/log/" + id)
-      .then((res) => setTicketLogs(res.data.data))
-      .catch((e) => console.log(e));
+      .then((res) => {
+        setTicketLogs(res.data.data);
+        setIsLoading(false);
+      })
+      .catch((e) => {
+        console.log(e);
+        setIsLoading(false);
+      });
   };
 
   useEffect(() => {
@@ -36,47 +44,51 @@ const ViewLog = ({ id }) => {
       </Typography>
       <hr />
       <Paper elevation={0}>
-        <List>
-          {ticketLogs?.length > 0 ? (
-            ticketLogs.map((log, i) => {
-              return (
-                <ListItem disablePadding key={i}>
-                  <ListItemButton>
-                    <ListItemIcon>
-                      <ArrowRightAltIcon />
-                    </ListItemIcon>
+        {isLoading ? (
+          <CircularProgress size={30} />
+        ) : (
+          <List>
+            {ticketLogs?.length > 0 ? (
+              ticketLogs.map((log, i) => {
+                return (
+                  <ListItem disablePadding key={i}>
+                    <ListItemButton>
+                      <ListItemIcon>
+                        <ArrowRightAltIcon />
+                      </ListItemIcon>
 
-                    <ListItemText
-                      // primary="John Recently Viewed This Record"
-                      secondary={
-                        <>
-                          <Typography
-                            sx={{ display: "inline" }}
-                            component="span"
-                            variant="body2"
-                            color="text.primary"
-                          >
-                            {log.agent.firstName} {log.agent.lastName} (
-                            {log.agent.employeeCode})
-                          </Typography>
-                          {`--${log.action} `}
-                          <span
-                            style={{ float: "right", width: "fit-content	" }}
-                          >
-                            {log.createdAt.split("T")[1]}{" "}
-                            {log.createdAt.split("T")[0]}
-                          </span>
-                        </>
-                      }
-                    />
-                  </ListItemButton>
-                </ListItem>
-              );
-            })
-          ) : (
-            <Typography variant="h6">No Logs found!!</Typography>
-          )}
-        </List>
+                      <ListItemText
+                        // primary="John Recently Viewed This Record"
+                        secondary={
+                          <>
+                            <Typography
+                              sx={{ display: "inline" }}
+                              component="span"
+                              variant="body2"
+                              color="text.primary"
+                            >
+                              {log.agent.firstName} {log.agent.lastName} (
+                              {log.agent.employeeCode})
+                            </Typography>
+                            {`--${log.action} `}
+                            <span
+                              style={{ float: "right", width: "fit-content	" }}
+                            >
+                              {log.createdAt.split("T")[1]}{" "}
+                              {log.createdAt.split("T")[0]}
+                            </span>
+                          </>
+                        }
+                      />
+                    </ListItemButton>
+                  </ListItem>
+                );
+              })
+            ) : (
+              <Typography variant="h6">No Logs found!!</Typography>
+            )}
+          </List>
+        )}
       </Paper>
     </>
   );
